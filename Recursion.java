22a87dba93d048cd7d31632b28f5a6b27216404f
SUJITP25 <sujitpatel1111@gmail.com>
2025-08-12 12:14:15 +0530

    new commit

--- Recursion.java
+++ Recursion.java
@@ -1,8 +1,24 @@
 public class Recursion {
+    public static int findFactorial(int n){
+        if (n<=1) return 1;
+        int factorial= n*findFactorial(n-1); 
+        return factorial;
+    }
+
+    // Given the Integer,find the sum of its digits using Recursion
+    public static int digitSum(int n){
+        if (n<=0) return 0;
+        int lastDigit = n % 10;
+        return lastDigit + digitSum(n/10);
+    }
+
+    
+
+    
     public static void main(String[] args) {
-        
+        System.out.println(findFactorial(5));
+        System.out.println(digitSum(125));
     }
 }
 
-// Recursion 
-//  
+
